1955e1fae4195302ad7b4f9d658a71307d476364
Mateus Gabriel <mateus.gabriel042@gmail.com>
2021-08-19 19:28:40 -0300

    Primeira atualização do conteudo.

--- index.php
+++ index.php
@@ -19,37 +19,38 @@
 				<img src="img/logo.png" class="logo">
 
 				<div class="redes-sociais">
-					<a href="#"><i class="fa">&#xf09a;</i></a><!--facebook-->
-					<a href="#"><i class="fa">&#xf0e1;</i></a><!--tuiter-->
-					<a href="#"><i class="fa">&#xf099;</i></a><!--facebook-->
-					<a href="#"><i class="fa">&#xf232;</i></a><!--whatsapp-->
-					<a href="#"><i class="fa">&#xf16d;</i></a><!--instagram-->
+					<a href="https://www.linkedin.com/company/kerosolar/"><i class="fa">&#xf0e1;</i></a><!--linkedin-->
+					<a href="https://www.facebook.com/kerosolartecnologia"><i class="fa">&#xf09a;</i></a><!--facebook-->
+					<a href="https://www.instagram.com/kerosolar/"><i class="fa">&#xf16d;</i></a><!--instagram-->
+					<a href="https://api.whatsapp.com/send/?phone=552141412089&text&app_absent=0"><i class="fa">&#xf232;</i></a><!--whatsapp-->
+					<a href="https://t.me/kerosolar"><i class="fa">&#xf1d8;</i></a><!--telegram-->
 				</div>
 			</div>
 			<content>
 				<div class="center-header">
 					<div class="area-info-header">
-						<h1>ADIQUIRA UMA USINA CONOSCO</h1>
+						<h1>SUA CONTA DE ENERGIA ESTÁ DOENDO NO BOLSO?</h1>
+						<h3>Troque sua conta de energia por um gerador solar ele se paga.</h3>
 
 						<ul>
 							<li>
-								<i class="fa">&#xf200;</i>*
-								<label>Lorem Ipsum</label>
-								<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+								<i class="fa">&#xf0e7;</i>
+								<label>Economia</label>
+								<p>Redução de até 95% no valor de sua de energia.</p>
 							</li>
 							<li>
-								<i class="fa">&#xf080;</i>*
-								<label>Lorem Ipsum</label>
-								<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+								<i class="fa">&#xf0d6;</i>
+								<label>Pagamento</label>
+								<p>Parcelamento em até 120 vezes com até 6 meses de carência.</p>
 							</li>
 							<li>
-								<i class="fa">&#xf1fe;</i>
-								<label>Lorem Ipsum</label>
-								<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+								<i class="fa">&#xf0d1;</i>
+								<label>Entrega</label>
+								<p>Frete grátis para todo Brasil.</p>
 							</li>
 						</ul>
 
-						<a href="#" class="link">Link aleatório</a>
+						<a href="https://kerosolar.com.br/contato/" class="link">Solicite seu orçamento ou uma consultoria gratuita</a>
 					</div>
 				</div>
 			</content>
@@ -57,61 +58,56 @@
 
 		<section class="media">
 			<content>
-				<h1>Lorem ipsun</h1>
+				<h1>Benefícios de um Gerador Solar</h1>
 
-				<ul class="list-media">
-					<li><img src="img/img_default.jpg"></li>
-					<li><img src="img/img_default.jpg"></li>
-					<li><img src="img/img_default.jpg"></li>
-					<li><img src="img/img_default.jpg"></li>
-				</ul>
+				<iframe class="video" src="https://www.youtube.com/watch?v=21Al3oE1SIg"></iframe>
 
 				<ul class="list-details">
 					<li>
-						<i class="fa">&#xf1cc;</i>
+						<i class="fa">&#xf0eb;</i>
 						<div>
-							<label>Lorem ipsum</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Energia</label>
+							<p>Redução direta e rápida do seu custo em energia.</p>
 						</div>
 					</li>
 
 					<li>
-						<i class="fa">&#xf1cc;</i>
+						<i class="fa">&#xf024;</i>
 						<div>
-							<label>Lorem ipsum</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Bandeiras</label>
+							<p>Fugir das bandeiras Amarelas e Vermelhas que entram em vigor com a diminuição dos reservatórios.</p>
 						</div>
 					</li>
 
 					<li>
-						<i class="fa">&#xf1cc;</i>
+						<i class="fa">&#xf201;</i>
 						<div>
-							<label>Lorem ipsum</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Fim dos reajustes</label>
+							<p>Não se preocupar mais com reajuste de energia (3 vezes ao ano no mínimo).</p>
 						</div>
 					</li>
 
 					<li>
-						<i class="fa">&#xf1cc;</i>
+						<i class="fa">&#xf015;</i>
 						<div>
-							<label>Lorem ipsum</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Imóvel</label>
+							<p>Valoriza seu imóvel de 10 a 15%.</p>
 						</div>
 					</li>
 
 					<li>
-						<i class="fa">&#xf1cc;</i>
+						<i class="fa">&#xf0d6;</i>
 						<div>
-							<label>Lorem ipsum</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>IPTU</label>
+							<p>Desconto no IPTU que é concedido em algumas cidades. (selo verde).</p>
 						</div>
 					</li>
 
 					<li>
-						<i class="fa">&#xf1cc;</i>
+						<i class="fa">&#xf063;</i>
 						<div>
-							<label>Lorem ipsum</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Economia com gás</label>
+							<p>Redução dos gastos com Gás. (utilização de eletrodomésticos elétricos).</p>
 						</div>
 					</li>
 				</ul>
@@ -120,104 +116,122 @@
 
 		<section class="models">
 			<content>
-				<h1>Modelos disponíveis</h1>
+				<h1>Topologias disponíveis</h1>
 
 				<ul class="list-models">
 					<li>
-						<img src="img/img_celular.png">
-						<label class="lb-price">R$ 4664</label>
+						<label class="name-product">Kit Micro Inversor de 1.80 Kwp</label>
+						<img src="img/kit_micro_inversor_de_1.80_Kwp.png">
+						<label class="obs">Não está incluso nos preços Homologação e Instalação!</label>
+						<div class="area-price">
+							<label class="lb-price">a partir de</label>
+							<label class="price">R$ 6.300,00</label>
+							<label class="lb-descont">ou 60x de R$ 188,06</label>
+						</div>
 						<ul class="list-details">
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
+							<li>04 Painéis de 450W mono cristalino</li>
+							<li>Estrutura telhado</li>
+							<li>Conectores MC.4</li>
+							<li>-</li>
 						</ul>
 					</li>
 
 					<li>
-						<img src="img/img_celular.png">
-						<label class="lb-price">R$ 4664</label>
+						<label class="name-product">Kit Inversor de string de 3.60 Kwp</label>
+						<img src="img/kit_inversor_de_string_de_3.60_kwp.jpg">
+						<label class="obs">Não está incluso nos preços Homologação e Instalação!</label>
+						<div class="area-price">
+							<label class="lb-price">a partir de</label>
+							<label class="price">R$ 11.600,00</label>
+							<label class="lb-descont">ou 60x de R$ 330,47</label>
+						</div>
 						<ul class="list-details">
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
+							<li>08 Painéis de 450W mono cristalino</li>
+							<li>String Box CC</li>
+							<li>50 metros de cabo solar</li>
+							<li>Estrutura telhado e conectores MC.4</li>
 						</ul>
 					</li>
 
 					<li>
-						<img src="img/img_celular.png">
-						<label class="lb-price">R$ 4664</label>
+						<label class="name-product">Kit Inversor de string de 8.10 Kwp</label>
+						<img src="img/weg_1000.jpg">
+						<label class="obs">Não está incluso nos preços Homologação e Instalação!</label>
+						<div class="area-price">
+							<label class="lb-price">a partir de</label>
+							<label class="price">R$ 26.600,00</label>
+							<label class="lb-descont">ou 60x de R$ 733,52</label>
+						</div>
 						<ul class="list-details">
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
-							<li>Lorem ipsun</li>
+							<li>18 painéis de 450W mono cristalino</li>
+							<li>String Box CC</li>
+							<li>150 metros de cabo solar</li>
+							<li>Estrutura telhado e conectores MC.4</li>
 						</ul>
 					</li>
 				</ul>
 				<div class="area-link">
-					<a href="#">Um link qualquer</a>
+					<a href="https://kerosolar.com.br/contato/">Solicite seu orçamento ou uma consultoria gratuita</a>
 				</div>
 			</content>
 		</section>
 
 		<section class="feedbacks">
 			<content>
-				<h1>O que falam sobre nós</h1>
+				<h1>Depoimentos de clientes atendidos</h1>
 
 				<ul class="list-feedbacks">
 					<li>
-						<img src="img/img_celular.png">
+						<i class="fa">&#xf2bd;</i>
 						<div class="text-feedback">
-							<label>Mateus Gabriel</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Luann Batista Henrique</label>
+							<p>Desde o princípio de cotações a compra, bem como no ato da instalação dos painéis, todos com esmero profissionalismo, deixo aqui os Parabéns pra toda a equipe, abraço.</p>
 						</div>
 					</li>
 
 					<li>
-						<img src="img/img_celular.png">
+						<i class="fa">&#xf2bd;</i>
 						<div class="text-feedback">
-							<label>Mateus Gabriel</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Maximino</label>
+							<p>Muito bom excelente. Total compromisso e bom trabalho e preço justo. Eu indico total organização com documentos tudo esclarecido e tudo no prazo.</p>
 						</div>
 					</li>
 
 					<li>
-						<img src="img/img_celular.png">
+						<i class="fa">&#xf2bd;</i>
 						<div class="text-feedback">
-							<label>Mateus Gabriel</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Edson Leite</label>
+							<p>O atendimento foi cordial e técnico, demonstrando como funciona o sistema e opções de instalaçao.</p>
 						</div>
 					</li>
 
 					<li>
-						<img src="img/img_celular.png">
+						<i class="fa">&#xf2bd;</i>
 						<div class="text-feedback">
-							<label>Mateus Gabriel</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Adriana Ferreira</label>
+							<p>Fui muito bem atendida, todas as dúvidas são esclaridas. Suporte total.</p>
 						</div>
 					</li>
 
 					<li>
-						<img src="img/img_celular.png">
+						<i class="fa">&#xf2bd;</i>
 						<div class="text-feedback">
-							<label>Mateus Gabriel</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Roger Aguiar</label>
+							<p>Excelente. Bom preço; equipamento moderno e execução rápida. Recomendo.</p>
 						</div>
 					</li>
 
 					<li>
-						<img src="img/img_celular.png">
+						<i class="fa">&#xf2bd;</i>
 						<div class="text-feedback">
-							<label>Mateus Gabriel</label>
-							<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+							<label>Simone</label>
+							<p>Foi muito boa, total atenção e qualidade ótima. Recomendo para todos.</p>
 						</div>
 					</li>
 				</ul>
+				<div class="area-link">
+					<a href="#">Confira todos os depoimentos</a>
+				</div>
 			</content>
 		</section>
 
@@ -227,25 +241,28 @@
 					<img src="img/img_partner_00.jpg" alt=""/>
 				</div>
 				<div class="item">
-					<img src="img/img_partner_00.jpg" alt=""/>
+					<img src="img/img_partner_01.jpg" alt=""/>
 				</div>
 				<div class="item">
-					<img src="img/img_partner_00.jpg" alt=""/>
+					<img src="img/img_partner_02.jpg" alt=""/>
 				</div>
 				<div class="item">
-					<img src="img/img_partner_00.jpg" alt=""/>
+					<img src="img/img_partner_03.jpg" alt=""/>
 				</div>
 				<div class="item">
-					<img src="img/img_partner_00.jpg" alt=""/>
+					<img src="img/img_partner_04.jpg" alt=""/>
 				</div>
 				<div class="item">
-					<img src="img/img_partner_00.jpg" alt=""/>
+					<img src="img/img_partner_05.jpg" alt=""/>
 				</div>
 				<div class="item">
-					<img src="img/img_partner_00.jpg" alt=""/>
+					<img src="img/img_partner_06.jpg" alt=""/>
 				</div>
 				<div class="item">
-					<img src="img/img_partner_00.jpg" alt=""/>
+					<img src="img/img_partner_07.jpg" alt=""/>
+				</div>
+				<div class="item">
+					<img src="img/img_partner_08.jpg" alt=""/>
 				</div>
 			</div>
 		</section>
@@ -255,18 +272,22 @@
 				<h1>Perguntas frequestes</h1>
 
 				<div>
-					<p class="question">O que é Lorem Ipsum?</p>
-					<p class="answer">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+					<p class="question">É caro para instalar um gerador solar?</p>
+					<p class="answer">Não, caro é pagar a conta de energia, O Kit pode ser financiado, as parcelas tem o preço abaixo do que se paga hoje na conta de energia. E com a vantagem de não ter aumento. A conta de energia aumenta 3 vezes ao Ano.</p>
 				</div>
 
 				<div>
-					<p class="question">O que é Lorem Ipsum?</p>
-					<p class="answer">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+					<p class="question">Em quanto tempo começarei a economizar?</p>
+					<p class="answer">Após instalação o prazo que a concessionária leva até a troca do seu medidor varia de 30 a 45 dias, mas tem ocorrido mais rápido. O equipamento estará Instalado e Homologado antes mesmo de pagar a primeira parcela.</p>
 				</div>
 
 				<div>
-					<p class="question">O que é Lorem Ipsum?</p>
-					<p class="answer">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+					<p class="question">Como saber o preço exato do meu gerador e parcelas?</p>
+					<p class="answer">Para dimensionar o seu gerador de forma exata precisamos que nos envie uma foto da sua conta de energia, Assim fazemos o cálculo pela média de seu consumo anual.</p>
+				</div>
+
+				<div class="area-link">
+					<a href="#">Solicite seu orçamento ou uma consultoria gratuita</a>
 				</div>
 			</content>
 		</section>
@@ -274,15 +295,15 @@
 		<footer>
 			<content>
 				<div class="redes-sociais">
-					<a href="#"><i class="fa">&#xf09a;</i></a><!--facebook-->
-					<a href="#"><i class="fa">&#xf0e1;</i></a><!--tuiter-->
-					<a href="#"><i class="fa">&#xf099;</i></a><!--facebook-->
-					<a href="#"><i class="fa">&#xf232;</i></a><!--whatsapp-->
-					<a href="#"><i class="fa">&#xf16d;</i></a><!--instagram-->
+					<a href="https://www.linkedin.com/company/kerosolar/"><i class="fa">&#xf0e1;</i></a><!--linkedin-->
+					<a href="https://www.facebook.com/kerosolartecnologia"><i class="fa">&#xf09a;</i></a><!--facebook-->
+					<a href="https://www.instagram.com/kerosolar/"><i class="fa">&#xf16d;</i></a><!--instagram-->
+					<a href="https://api.whatsapp.com/send/?phone=552141412089&text&app_absent=0"><i class="fa">&#xf232;</i></a><!--whatsapp-->
+					<a href="https://t.me/kerosolar"><i class="fa">&#xf1d8;</i></a><!--telegram-->
 				</div>
 				<div class="infos-rodape">
-					<label>Nome da empresa © Todos os direitos reservados</label>
-					<label>CNPJ: 42.451.210/0001-46 | (84) 99999 - 9999</label>
+					<label>Kerosolar 2021 © Todos os direitos reservados</label>
+					<label>Contato: +55 (21) 4141-2089 | Email: kerosolar@kerosolar.com.br</label>
 				</div>
 			</content>
 		</footer>
